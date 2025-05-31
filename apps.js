eb43b97bf81ae03746c209011864aa6af3555e6d
a <a>
2025-05-31 15:35:27 +0500

    initial commit

---
+++ apps.js
@@ -0,0 +1,7 @@
+const buttons = querySelectorAll(".button")
+buttons.forEach((button) => {
+    button.addEventListener("click" , ()  => {
+        const element = document.documentElement
+        element.dataset.theme = button.dataset.theme
+    })
+})
